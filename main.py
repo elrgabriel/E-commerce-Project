d929561f736575512cfe31dda6b23a0a67b6f0ee
Euller_Gabriel <eullergoliveira@gmail.com>
2024-11-20 14:27:18 +0000

    Atualizei a file 404.html para reaproveitar codigo já da base.html routing para 404.html feito e adicionato nas anchor tags do header e footer

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template # type: ignore
 from data import items
 
 app = Flask(__name__)
 
 
 @app.route('/')
+@app.route('/home')
 def index():
     return render_template('index.html', items = items)
+
+@app.route('/404')
+def error():
+    return render_template('404.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
